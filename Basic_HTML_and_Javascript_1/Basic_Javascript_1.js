170270f93f500b4252104e6759a2684d9ec29475
Ddenney2 <60047265+Ddenney2@users.noreply.github.com>
2020-01-22 07:44:25 -0800

    JavaScript Alert
    Changed to just the alert.

--- Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
+++ Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
@@ -1,13 +1,3 @@
 <!DOCTYPE HTML>
-<html lang="en">
-	<head>
-		<title>Work</title>
-		<meta charset="uft-8">
-		<script>
-			alert=("Hello, World.");
-		</script>
-	</head>
-<body>
-	<p>Words</p>
-</body>
-</html>
+alert=("Hello, World.");
+	
