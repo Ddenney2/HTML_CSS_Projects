f2e516650feb435d043f6e9ac90fceeb06889d25
Ddenney2 <60047265+Ddenney2@users.noreply.github.com>
2020-01-22 07:02:32 -0800

    HTML and JavaScript
    Combining the two languages.

---
+++ Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<title>Work</title>
+		<meta charset="uft-8">
+		<script>
+			alert=("Hello, World.");
+		</script>
+	</head>
+<body>
+	<p>Words</p>
+</body>
+</html>
